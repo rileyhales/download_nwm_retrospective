4e38a8d704cdb81893a5f43eb84366f135266267
Riley Hales <rileyhales1@gmail.com>
2022-08-28 17:58:39 -0600

    delete comments and prints

--- nwm_to_daily.py
+++ nwm_to_daily.py
@@ -14,15 +14,12 @@ import sys
 if __name__ == "__main__":
     year = int(sys.argv[1])
 
-    # convert this code to bash
     daterange = pd.date_range(
         datetime.date(year, 1, 1),
         datetime.date(year, 12, 31),
         freq="D"
     )
     for date in daterange:
-        print(date)
         wildcard_path = os.path.join(".", f"{year}_slim", f"{date.strftime('%Y%m%d')}*")
-        print(wildcard_path)
         subprocess.call(f'nces {wildcard_path} -o {year}_slim_daily/{date.strftime("%Y%m%d")}.nc --op_typ mean', shell=True)
         subprocess.call(f'rm {wildcard_path}', shell=True)
