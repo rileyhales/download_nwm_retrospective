46b7c9c1e58cab5d732cdc529e36ca7842862a02
Riley Hales <rileyhales1@gmail.com>
2022-08-28 17:39:15 -0600

    delete files as they are processed

--- nwm_drop_variables.py
+++ nwm_drop_variables.py
@@ -7,3 +7,4 @@ if __name__ == '__main__':
     year = int(sys.argv[1])
     for nc in glob.glob(os.path.join(f'{year}', '*')):
         subprocess.call(f'ncks -v streamflow {nc} {year}_slim/{os.path.basename(nc)}', shell=True)
+        subprocess.call(f'rm {nc}', shell=True)
