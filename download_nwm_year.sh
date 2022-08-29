1d5dd5d6e048a7d8ca0d45a49d8b9c0fe42924df
Riley Hales <rileyhales1@gmail.com>
2022-08-28 23:36:15 -0600

    swap order of averaging and dropping variables

--- download_nwm_year.sh
+++ download_nwm_year.sh
@@ -10,7 +10,12 @@ echo "Year chosen is $1"
 # download hourly
 echo "Downloading hourly data"
 mkdir "$1"
-aws s3 cp s3://noaa-nwm-retrospective-2-1-pds/model_output/$1 ./$1 --no-sign-request --recursive --exclude "*" --include "*CHRTOUT_DOMAIN1.comp"
+aws s3 cp s3://noaa-nwm-retrospective-2-1-pds/model_output/$1 ./$1 --no-sign-request --recursive --exclude "*" --include "198001*CHRTOUT_DOMAIN1.comp"
+
+# convert to daily
+echo "Converting to daily"
+mkdir "$1_daily"
+python nwm_to_daily.py "$1"
 
 # copy only streamflow variable to new files
 echo "Dropping variables"
@@ -21,11 +26,6 @@ python nwm_drop_variables.py $1
 echo "Deleting hourly data"
 rm -r "$1"
 
-# convert to daily
-echo "Converting to daily"
-mkdir "$1_slim_daily"
-python nwm_to_daily.py "$1"
-
 # zip for export
 echo "Zipping"
-zip -r "$1.zip" "$1_slim_daily/"
+zip -r "$1.zip" "$1_slim"
--- nwm_drop_variables.py
+++ nwm_drop_variables.py
@@ -5,6 +5,6 @@ import sys
 
 if __name__ == '__main__':
     year = int(sys.argv[1])
-    for nc in glob.glob(os.path.join(f'{year}', '*')):
+    for nc in glob.glob(os.path.join(f'{year}_daily', '*')):
         subprocess.call(f'ncks -v streamflow {nc} {year}_slim/{os.path.basename(nc)}', shell=True)
         subprocess.call(f'rm {nc}', shell=True)
