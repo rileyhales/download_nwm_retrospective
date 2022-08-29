f778b31db4b3dd54b2c4e286d5ba0da06f0d4808
Riley Hales <rileyhales1@gmail.com>
2022-08-28 23:39:31 -0600

    remove directories after zipping

--- download_nwm_year.sh
+++ download_nwm_year.sh
@@ -10,7 +10,7 @@ echo "Year chosen is $1"
 # download hourly
 echo "Downloading hourly data"
 mkdir "$1"
-aws s3 cp s3://noaa-nwm-retrospective-2-1-pds/model_output/$1 ./$1 --no-sign-request --recursive --exclude "*" --include "198001*CHRTOUT_DOMAIN1.comp"
+aws s3 cp s3://noaa-nwm-retrospective-2-1-pds/model_output/$1 ./$1 --no-sign-request --recursive --exclude "*" --include "*CHRTOUT_DOMAIN1.comp"
 
 # convert to daily
 echo "Converting to daily"
@@ -22,10 +22,12 @@ echo "Dropping variables"
 mkdir "$1_slim"
 python nwm_drop_variables.py $1
 
-# delete hourly
-echo "Deleting hourly data"
-rm -r "$1"
-
 # zip for export
 echo "Zipping"
 zip -r "$1.zip" "$1_slim"
+
+# remove hourly and daily files
+echo "Removing hourly and daily files"
+rm -r "$1"
+rm -r "$1_daily"
+rm -r "$1_slim"
