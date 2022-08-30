12b42c09c0df2e0b42d9599aa9426b277ae10c2a
Riley Hales <rileyhales1@gmail.com>
2022-08-29 21:40:44 -0600

    uncomment

--- download_nwm_year.sh
+++ download_nwm_year.sh
@@ -6,26 +6,24 @@
 
 echo "Year chosen is $1"
 
-
 # download hourly
 echo "Downloading hourly data"
-#mkdir "$1"
-#aws s3 cp s3://noaa-nwm-retrospective-2-1-pds/model_output/$1 ./$1 --no-sign-request --recursive --exclude "*" --include "19800101*CHRTOUT_DOMAIN1.comp"
-#cp "$1" "$1_copy"
+mkdir "$1"
+aws s3 cp s3://noaa-nwm-retrospective-2-1-pds/model_output/$1 ./$1 --no-sign-request --recursive --exclude "*" --include "19800101*CHRTOUT_DOMAIN1.comp"
 
 # convert to daily
-#echo "Converting to daily"
-#mkdir "$1_daily"
-#python nwm_to_daily.py "$1"
+echo "Converting to daily"
+mkdir "$1_daily"
+python nwm_to_daily.py "$1"
 
 # copy only streamflow variable to new files
-#echo "Dropping variables"
-#mkdir "$1_slim"
-#python nwm_drop_variables.py $1
+echo "Dropping variables"
+mkdir "$1_slim"
+python nwm_drop_variables.py $1
 
 # zip for export
-#echo "Zipping"
-#zip -r "$1.zip" "$1_slim"
+echo "Zipping"
+zip -r "$1.zip" "$1_slim"
 
 # remove hourly and daily files
 echo "Removing hourly and daily files"
